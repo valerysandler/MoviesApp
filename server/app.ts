c91dfc0100e1883675f5ca479da8919e0424d0ef
Valery Sandler <83174159+valerysandler@users.noreply.github.com>
2025-08-07 22:37:43 +0300

    express and ts implement, express server added

---
+++ server/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('API is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
